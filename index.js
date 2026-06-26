568db52eb2bf5421e0759d328c9fd2b37a44040d
Aman Sahu <amansahuxy@gmail.com>
2026-06-26 12:15:37 +0530

    Initialize BMI calculator project structure

---
+++ index.js
@@ -0,0 +1,6 @@
+const Weight=document.getElementById("weight");
+const Height=document.getElementById("height");
+const button=document.getElementById("btn");
+const Result=document.getElementById("result");
+
+   
