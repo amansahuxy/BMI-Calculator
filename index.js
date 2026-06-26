307297bbb0f9ef5ac518906d0b060c18e605c500
Aman Sahu <amansahuxy@gmail.com>
2026-06-26 12:32:47 +0530

    Implement BMI calculation logic

--- index.js
+++ index.js
@@ -3,4 +3,13 @@ const Height=document.getElementById("height");
 const button=document.getElementById("btn");
 const Result=document.getElementById("result");
 
-   
+   button.addEventListener("click",()=>{
+    let wght=Number(Weight.value);
+    let hght=Number((Height.value)/100);
+    let rslt=wght/(hght*hght);
+    Result.innerText=rslt;
+
+    if(rslt<18.5){
+        
+    }
+   })
